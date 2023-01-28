b36db02f2e22238fa13c37a7ab228242ba3a735d
danielalonso77 <54615444+danielalonso77@users.noreply.github.com>
2023-01-28 19:56:41 +0000

    Primer Commit en codespac de github

---
+++ Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ1gPUMB0OT4w-cwc1TSwnFmOvfUhF5dY",
+  authDomain: "alumnospasede-lista.firebaseapp.com",
+  projectId: "alumnospasede-lista",
+  storageBucket: "alumnospasede-lista.appspot.com",
+  messagingSenderId: "310098351409",
+  appId: "1:310098351409:web:b288a530dc691647fa4c10"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ backend.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase/app';
+
+const alumnosRef = firebase.firestore().collection('alumnos');
+
+// agregarAlumno
+
+alumnosRef.add({
+    matricula:'IA-0001',
+    nombre: 'Juan',
+    apellido: 'Perez',
+    edad: 20,
+    correo : 'juanperez@mail.com',
+    telefono: '1234567890'}
+)
+
+// obtenerAlumnos
+alumnosRef.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
+
+//crear una collection para asistencia de alumnos
+const asistenciaRef = firebase.firestore().collection('asistencia');
+
+// agregarAsistencia
+asistenciaRef.add({
+    matricula:'IA-0001',
+    fecha: '2021-09-01',
+    asistencia: true
+})
